2f305c1a343c77af64354212b086fcc618419af4
Aditya <aditya007r29@gmail.com>
2025-02-04 14:48:49 +0530

    DONE

--- Frontend/vite-project/src/App.jsx
+++ Frontend/vite-project/src/App.jsx
@@ -1,18 +1,19 @@
-import { BrowserRouter,Routes,Route } from "react-router-dom"; 
-import { LoginPage,SignupPage} from "./Routes.jsx"
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { LoginPage, SignupPage, Home, CreateProduct } from "./Routes";
+import "./App.css";
 
-import './App.css';
-
-function App(){
-  return(
-    <BrowserRouter>
-      <Routes>
-        <Route path="/Login" element={<LoginPage/>}/>
-        <Route path="/Signup" element={<SignupPage/>}/>
-
-      </Routes>
-    </BrowserRouter>
-  )
-}
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/login' element={<LoginPage />} />
+                <Route path='/signup' element={<SignupPage />} />
+                <Route path='/create-product' element={<CreateProduct />} />
+            </Routes>
+        </BrowserRouter>
+    );
+};
 
 export default App;
--- Frontend/vite-project/src/Routes.jsx
+++ Frontend/vite-project/src/Routes.jsx
@@ -1,4 +1,6 @@
-import LoginPage from './pages/Login';
-import SignupPage from './Pages/Signup';
+import LoginPage from "./pages/Login";
+import SignupPage from "./pages/Signup";
+import Home from "./pages/Home";
+import CreateProduct from "./Pages/createProduct.jsx";
 
-export { LoginPage, SignupPage };
+export { LoginPage, SignupPage, Home, CreateProduct };
